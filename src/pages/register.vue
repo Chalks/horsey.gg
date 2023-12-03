2fee266745af3beab98030f87cd7093296f98192
Jonathan Walters <skychalk@gmail.com>
2023-12-03 16:44:52 -0500

    starting to wire up sjwt

---
+++ src/pages/register.vue
@@ -0,0 +1,55 @@
+<script setup>
+import {useSjwtStore} from 'store/sjwt.js';
+
+const sjwtStore = useSjwtStore();
+const email = ref('');
+const password = ref('');
+
+const register = async (e) => {
+    e.preventDefault();
+    const response = await sjwtStore.register(email.value, password.value);
+    console.log(response);
+};
+
+</script>
+
+<template>
+    <div class="prose">
+        <form @submit="register">
+            <h3>Register</h3>
+            <p>
+                Your horsey.gg account will enable you to track stats, and if you
+                want we'll notify you when new game modes are available.
+            </p>
+
+            <label for="email" class="cursor-pointer">Email</label>
+            <input
+                id="email"
+                v-model="email"
+                type="email"
+                name="email"
+                placeholder="Email"
+            />
+
+            <label for="password" class="cursor-pointer">Password</label>
+            <input
+                id="password"
+                v-model="password"
+                type="password"
+                name="password"
+                placeholder="********"
+            />
+
+            <div class="flex items-center">
+                <input id="subscribe" type="checkbox" class="w-auto mr-4" checked />
+                <label for="subscribe" class="cursor-pointer">
+                    Tell me when there are new horseys to play with
+                </label>
+            </div>
+
+            <input type="submit" value="Register" />
+        </form>
+    </div>
+</template>
+
+
