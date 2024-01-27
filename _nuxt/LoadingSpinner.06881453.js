f8dedb439411ee4fc246f2857373f92b93d18e18
publish action <publish-action@github.com>
2024-01-27 16:19:09 +0000

    deploy: v1.0.10

--- _nuxt/LoadingSpinner.06881453.js
+++
@@ -1,1 +0,0 @@
-import{E as e,o as _,c as n,u as o,I as c}from"./entry.b37d9db1.js";const s={__name:"LoadingSpinner",setup(t){return(a,r)=>(_(),n(o(c),{icon:"mingcute:toy-horse-line",class:"rock"}))}},i=e(s,[["__scopeId","data-v-2ece8d85"]]);export{i as _};
