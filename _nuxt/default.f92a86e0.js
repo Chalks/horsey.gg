ac753f615cbf7dcaa21e13fb39c0c867015cd27d
publish action <publish-action@github.com>
2024-02-03 18:31:52 +0000

    deploy: v1.0.14

--- _nuxt/default.f92a86e0.js
+++
@@ -1,1 +0,0 @@
-import{J as o,o as s,f as t,i as r}from"./entry.714c30ec.js";const a={},c={class:"prose prose-blue mx-auto"};function n(e,l){return s(),t("div",c,[r(e.$slots,"default")])}const f=o(a,[["render",n]]);export{f as default};
