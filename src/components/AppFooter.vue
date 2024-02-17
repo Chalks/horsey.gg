85685e83334c13e639896329191c605eb554a003
Jonathan Walters <skychalk@gmail.com>
2024-02-17 15:23:18 -0500

    feat: cleaner ui

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,6 +3,9 @@ const year = new Date().getUTCFullYear();
 </script>
 
 <template>
+    <div class="max-w-prose mx-auto flex justify-end items-end w-full">
+        <HorsefaceIcon large class="text-horse" />
+    </div>
     <div class="bg-gradient-to-r from-gray-700 via-gray-900 to-gray-700 text-gray-100 px-4 py-4 md:py-12">
         <div class="max-w-prose mx-auto flex justify-between items-center">
             <span class="text-sm">Copyright © {{ year }}</span>
