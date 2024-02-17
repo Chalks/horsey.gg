6c63a9336d1077a08c5bb032dee30c445e793628
Jonathan Walters <skychalk@gmail.com>
2024-02-17 14:31:31 -0500

    feat: get the horse resources in

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,26 +45,9 @@ export default defineNuxtConfig({
             link: [
                 {
                     rel: 'icon',
-                    type: 'image/x-icon',
-                    href: '/favicon.ico',
-                },
-                {
-                    rel: 'icon',
-                    type: 'image/png',
-                    sizes: '16x16',
-                    href: '/favicon-16x16.png',
-                },
-                {
-                    rel: 'icon',
-                    type: 'image/png',
-                    sizes: '32x32',
-                    href: '/favicon-32x32.png',
-                },
-                {
-                    rel: 'icon',
                     type: 'image/png',
                     sizes: '96x96',
-                    href: '/favicon-96x96.png',
+                    href: '/horsey.png',
                 },
                 {
                     rel: 'preload',
