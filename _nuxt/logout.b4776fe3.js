25336eecc42e906a31a479322518fb9f1dd3f4cc
publish action <publish-action@github.com>
2024-03-31 13:21:57 +0000

    deploy: v1.1.2

--- _nuxt/logout.b4776fe3.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./seo.2f37e6db.js";import{k as t,V as s,a,o as l,f as n,y as r,l as o}from"./entry.e5c49017.js";const u=o("h1",null,"Logout",-1),c=o("p",null,"Why would you do that.",-1),h={__name:"logout",setup(_){return e({pageTitle:"logout - horsey.gg"}),t(async()=>{await s(),a().setUser(null)}),(p,g)=>(l(),n(r,null,[u,c],64))}};export{h as default};
