d6e86ae9cd96c050413c7649b000e18c03d43ae8
Jonathan Walters <skychalk@gmail.com>
2024-03-31 09:20:54 -0400

    feat: add fade-in for first render

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,21 @@
+<script setup>
+const hidden = ref(true);
+
+onMounted(() => {
+    hidden.value = false;
+});
+</script>
 <template>
     <NuxtLayout>
+        <Transition
+            enter-active-class="duration-300 ease-out opacity-0"
+            enter-to-class="opacity-100"
+            leave-active-class="duration-200 ease-in"
+            leave-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+            <div v-if="hidden" class="fixed inset-0 z-[1000] bg-white" />
+        </Transition>
         <NuxtPage />
     </NuxtLayout>
 </template>
